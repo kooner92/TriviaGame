97ef18f2f3bedb77d31caf90b51a981f7ff657b6
Justin Kooner <kooner92@gmail.com>
2018-08-30 23:32:49 -0700

    still needs work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,55 +4,55 @@ $(document).ready(function() {
 // creat object with questions, options, and answers
   var correct = 0;
   var wrong = 0;
+  
   var intervalID;
-  var time = 30;
+  var time = 60;
   var clockRunning = false;
 
 var trivia = [
   {
     question: 'How many fluid ounces are in a pint?',
-    options: ['16', '24', '32', '12'],
+    options: ['16 ', '24 ', '32 ', '12 '],
     answer: '16',
   },
   {
     question: 'What city does Batman live in?',
-    options: ['Boston', 'LA', 'Gotham City', 'Krypton'],
+    options: ['Boston ', 'LA ', 'Gotham City ', 'Krypton '],
     answer: 'Gotham City',
   },
   {
     question: 'Which degrees is the same in celsius and in fahrenheit?',
-    options: ['40', '0', '32', '-40'],
+    options: ['40 ', '0 ', '32 ', '-40 '],
     answer: '-40',
   },
   {
     question: 'How many colors are in a rainbow?',
-    options: ['9', '6', '7', '8'],
+    options: ['9 ' , '6 ' , '7 ' , '8 '],
     answer: '7',
   },
   {
     question: "In Japanese, what is the word for goodbye?",
-    options: ['Chow','Sayonara','Konnichiwa','adios'],
+    options: ['Chow ','Sayonara ','Konnichiwa ','adios '],
     answer: 'Sayonara',
   },
   {
     question: 'Which fingernail grows fastest?',
-    options: ['Middle','Thumb','Pinky','Ring'],
+    options: ['Middle ','Thumb ','Pinky ','Ring '],
     answer: 'Middle',
   },
   {
     question: "What year did World War 1 start?",
-    options: ['1910', '1914', '1921','1912'],
+    options: ['1910 ', '1914 ', '1921 ','1912 '],
     answer: '1914',
   }];
 
 // #start on click function and begin clock
 
   $("#start").on("click", function () {
-    console.log(trivia[3].question)
+    console.log(trivia[1].question)
+    event.preventDefault();
+    // run();
 
-    // push first question to DOM
-
-    $("#question").push(trivia[3].question)
     clockRunning = true;
 
     if (clockRunning = true) {
@@ -69,6 +69,10 @@ var trivia = [
     if (time === 0) {
       reset();
     }
+
+    // function run() {
+    //   if ()
+    // }
     
     // function reset to stop clock at 0
 
@@ -76,6 +80,41 @@ var trivia = [
       clockRunning = false;
       clearInterval(intervalID);
     }
+
+
+    // push first question to DOM
+
+    $("#question").html(trivia[1].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[1].options+"<br>"+"</br>")
+    
+    $("#question").html(trivia[2].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[2].options+"<br>"+"</br>")
+    
+    $("#question").html(trivia[3].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[3].options+"<br>"+"</br>")
+    
+    $("#question").html(trivia[4].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[4].options+"<br>"+"</br>")
+    
+    $("#question").html(trivia[5].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[5].options+"<br>"+"</br>")
+    
+    $("#question").html(trivia[6].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[6].options+"<br>"+"</br>")
+    
+    $("#question").html(trivia[7].question+"<br>"+"</br>")
+    $("#options").prepend(trivia[7].options+"<br>"+"</br>")
+    
+   console.log(trivia.questions)
+   console.log(trivia.options)
+
+	// for (var i = 0; i < options.length; i++) {
+	// 	var option = options[i];
+	// 	console.log(options);
+  
+  // for (var i = 0; i < question.length; i++) {
+	// 	var q1 = question[i];
+	// 	console.log(question);
 
   })
 
